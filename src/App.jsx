71ea8f6cf2733fd8e1ba99aa8f6e5562f7c20a66
JayIrv <135241031+JIrv30@users.noreply.github.com>
2023-08-22 00:48:23 +0100

    Added Homepage and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Homepage from './Homepage'
 
 import './App.css'
 
@@ -7,7 +8,7 @@ function App() {
 
   return (
     <>
-      Something
+      <Homepage/>
     </>
   )
 }
---
+++ src/Homepage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './homepage.css'
+function Homepage () {
+
+  return (
+    <div className="main">
+      <h1 className="header">Quizzical</h1>
+      <p className="description">Test your general knowledge skills</p>
+      <button className="start-quiz-button"> Start Quiz</button>
+    </div>
+  )
+}
+
+export default Homepage
